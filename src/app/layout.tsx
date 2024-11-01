c8cddb3b65673435340f2c5daa1d26cd54d8ff74
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-01 07:45:28 -0300

    add Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.variable}>{children}</body>
+    <html lang="pt-br" className={inter.variable}>
+      <body>{children}</body>
     </html>
   )
 }
