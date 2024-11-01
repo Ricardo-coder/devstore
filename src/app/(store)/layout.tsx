d930ec50e77ea0f23d972d4c66cc782b4cf7ed79
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-01 08:21:39 -0300

    Header styling

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,9 +3,9 @@ import { type ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
       <Header />
       <main>{children}</main>
-    </>
+    </div>
   )
 }
