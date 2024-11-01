b4e35f4a7c9a92730599abd1d4c411384bd1d309
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-01 12:36:00 -0300

    include app handler to pdp

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,8 +1,6 @@
 import data from '../data.json'
 
 export async function GET() {
-  await new Promise((resolve) => setTimeout(resolve, 4000))
-
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return Response.json(featuredProducts)
