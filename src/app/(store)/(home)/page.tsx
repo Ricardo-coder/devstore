6b4a35ae14278d2c8431582523e39bd0826908e2
Ricardo Freitas <ricardo.freitas@acct.global>
2024-11-01 11:06:36 -0300

    home loading skeleton

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 async function getFeaturedProducts(): Promise<Array<Product>> {
-  const response = await api('/products/featured')
+  const response = await api('/products/featured', {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  })
   const products = await response.json()
 
   return products
